ec81dff4359a05a3f56553ec6d28a9775f24020e
2sagarraut2 <2sagarraut2@gmail.com>
2025-07-14 21:07:23 +0530

    Product list first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <h2>App</h2>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
+
+// multiple line code - block of code { return() }
+// single line - ()
