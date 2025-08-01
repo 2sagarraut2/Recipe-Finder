41c4e76e472b3af48b25fcc43fc01848c5f8d7aa
2sagarraut2 <2sagarraut2@gmail.com>
2025-08-01 07:50:43 +0530

    comments added

--- src/components/RecipeCardHOC.js
+++ src/components/RecipeCardHOC.js
@@ -1,9 +1,12 @@
 const RecipeCardHOC = (WrappedComponent) => {
+  //   accepting old component
   return function EnhancedComponent(props) {
+    // HOC component returns a function which returns a component and accepting props passed to old component
     return (
       <>
         <span className="calorie-label">Low Calorie</span>
         <WrappedComponent {...props} />
+        {/* rendering old component as it is */}
       </>
     );
   };
